02a63d9308ed6616cb17d5199367859a096345a9
yuslamtech <52999302+yuslamtech@users.noreply.github.com>
2020-08-26 02:07:03 -1200

    Add files via upload

---
+++ yuslam/basic.js
@@ -0,0 +1,17 @@
+var a=1,b=3,c=4;
+console.log(a+b+c);
+
+var habeeb = {
+    name: "habibullah",
+    course: "cs",
+    age: 22,
+    lang: "egbira",
+    girlfriend: {
+        girl1: "zarah",
+        girl2: "khadeeja"
+    },
+    getinfo: function(){
+        return this.name + "has 2 girlfriends namely" + this.girlfriend;
+    }
+};
+getinfo;
